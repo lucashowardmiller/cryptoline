8a56e09965da429e4bfb2c7b2f366521f6ec965e
lucashowardmiller <Lucas.h.miller@ndsu.edu>
2018-12-18 02:50:22 -0600

    added mirror support

--- cryptoline_modules/decode_cipher.py
+++ cryptoline_modules/decode_cipher.py
@@ -13,15 +13,19 @@ def decode_multilayer(cipher: str, max_layers: int):
 
 def decode_layer(cipher: str):
     """Used to solve a single layer cipher"""
-    if is_rot13(cipher):
-        return decode_rot13(cipher)
+
     if is_base64(cipher):
         return decode_base64(cipher)
     if is_base32(cipher):
         return decode_base32(cipher)
     if is_base16(cipher):
         return decode_base16(cipher)
-
+    if is_base85(cipher):
+        return decode_base85(cipher)
+    if is_mirror(cipher):
+        return decode_mirror(cipher)
+    if is_rot13(cipher):
+        return decode_rot13(cipher)
 
 def decode_rot13(cipher: str):
     """Used to decode rot13"""
@@ -68,12 +72,44 @@ def decode_base16(cipher: str):
 
 
 def is_base16(cipher: str):
-    """Used to solve a layer of base16"""
+    """Used to solve a layer of binary"""
     return detect_english.has_english(decode_base16(cipher))
 
 
-def decode_ceaser(cipher: str):
-    print("cat")
+def decode_base85(cipher: str):
+    try:
+        return base64.a85decode(cipher).decode("utf-8")
+    except:
+        return ""
 
-def decode_morse(cipher: str):
-    print("morse")
+
+def is_base85(cipher: str):
+    return detect_english.has_english(decode_base85(cipher))
+
+
+def decode_binary(cipher: str):
+    # TODO binary machine broke
+    try:
+
+        print("good ol go at it sport")
+    except:
+        return ""
+    print("here comes the binary ninja")
+
+
+def decode_mirror(cipher: str):
+    """Flips the ciphertext. eg. tac => cat """
+    return cipher[::-1]
+
+def is_mirror(cipher: str):
+    return detect_english.has_english(cipher)
+
+
+def is_binary(cipher: str):
+    """Used to solve a layer of binary"""
+    # TODO see decode_binary() should function once written
+    return detect_english.has_english(decode_binary(cipher))
+
+
+def decode_morse():
+    print("morsy mo")
