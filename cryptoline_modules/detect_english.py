7a2738b5d7677fc796c88aac8e7a9115dba66a0d
lucashowardmiller <Lucas.h.miller@ndsu.edu>
2018-12-20 23:37:50 -0600

    Improved docstrings

--- cryptoline_modules/detect_english.py
+++ cryptoline_modules/detect_english.py
@@ -3,8 +3,16 @@
 import re
 
 
+def load_words():
+    """Loads the words document into a set"""
+    with open('cryptoline_modules/words.txt') as word_file:
+        valid_words = set(word_file.read().lower().split())
+
+    return valid_words
+
+
 def has_english(string: str):
-    """Detects if a string (separated by anything) has an english word"""
+    """True if a string (separated by anything) has an english word"""
 
     not_letter = re.compile('[^0-9a-zA-Z]+')
     string = not_letter.sub(' ', string)
@@ -18,11 +26,3 @@ def has_english(string: str):
 def is_word(word: str):
     """If a string is a word, returns True, else False"""
     return word.lower() in load_words()
-
-
-def load_words():
-    """Loads the words document into a set"""
-    with open('cryptoline_modules/words.txt') as word_file:
-        valid_words = set(word_file.read().lower().split())
-
-    return valid_words
